553baf93c0b1b88cee6511808848c1e1b4a0bbeb
priyavarshini20 <priyavarshini383@gmail.com>
2024-11-18 21:17:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// import express
+const express = require('express');
+const Reciperoutes = require('./routes/Reciperoutes');
+const Recipe = require('./models/Recipe');
+
+// create an express app
+const app = express();
+
+// add middleware to parse JSON
+app.use(express.json());
+
+// define the root route
+app.use('/api/v1/routes',Reciperoutes);
+
+// export the app
+module.exports = app;
